dc2ee151188ae764c50f189e5d412a96639300d4
Satoshi Ishii <ishii@springboard-inc.jp>
2019-11-29 17:19:25 +0900

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Footer = () => (
+  <footer>
+    © {new Date().getFullYear()}, Built with
+    {` `}
+    <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <p>My Blog</p>
+  </footer>
+)
+
+export default Footer
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 const Aboutpage = () => (
-    <Layout>
-        <SEO title="About" />
-        <h1>About</h1>
-        <p>This page is About Page.</p>
-        <Link to="/">Go back to the homepage</Link>
-    </Layout>
+  <Layout>
+    <SEO title="About" />
+    <h1>About</h1>
+    <p>This page is About Page.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
 )
 
 export default Aboutpage
